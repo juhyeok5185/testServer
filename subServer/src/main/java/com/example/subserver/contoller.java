84c3eeab8c04f897c1015164ceda26f90a6cf4c7
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-08-25 18:34:53 +0900

    added

--- subServer/src/main/java/com/example/subserver/contoller.java
+++ subServer/src/main/java/com/example/subserver/contoller.java
@@ -1,7 +1,13 @@
 package com.example.subserver;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class contoller {
@@ -11,5 +17,20 @@ public class contoller {
         return "main";
     }
 
+//    @PostMapping("/main")
+//    public ResponseEntity<String> test() {
+//        System.out.println(requestBody);
+//        Integer id = (Integer) requestBody.get("id");
+//        String username = (String) requestBody.get("username");
+//        String password = (String) requestBody.get("password");
+//        String name = (String) requestBody.get("name");
+//
+//        System.out.println("id: " + id);
+//        System.out.println("username: " + username);
+//        System.out.println("password: " + password);
+//        System.out.println("name: " + name);
+//
+//        return ResponseEntity.ok("ddd");
+//    }
 }
 
--- subServer/src/main/resources/static/js/jsUtil.js
+++ subServer/src/main/resources/static/js/jsUtil.js
@@ -1,10 +1,45 @@
-console.log("dd");
 
-
-function find(target){
+function Find(target){
     return document.querySelector(target);
 }
 
-function findAll(target){
+function FindAll(target){
     return document.querySelectorAll(target);
+}
+
+function LogList(targets){
+    targets.forEach(function(target) {
+        console.log(target.value);
+    });
+}
+
+function LogMap(targets){
+    for (const key in targets) {
+        if (targets.hasOwnProperty(key)) {
+            console.log(`${key} : ${targets[key]}`);
+        }
+    }
+}
+
+function LogFormData(formData){
+    for (let pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
+    }
+}
+
+function ApiRequestBody(url, method, requestBody) {
+    fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data); // 서버 응답 데이터 출력
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
