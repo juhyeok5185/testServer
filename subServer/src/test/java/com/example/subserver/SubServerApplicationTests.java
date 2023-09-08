3f4e036e03874fe7c6a512c928851e0707e9971c
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-09-08 09:20:11 +0900

    added

---
+++ subServer/src/test/java/com/example/subserver/SubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
